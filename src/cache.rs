2870d66449a6863ed1173ccd94922ea0f7ae452a
Marcin Puc <tranzystorek.io@protonmail.com>
2022-12-01 23:50:53 +0100

    feat(wip): add basic cache module

---
+++ src/cache.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+
+use crate::paths;
+
+pub enum Entry {
+    /// A file open for reading that represents a cache hit
+    Cached(File),
+
+    /// A file open for writing that represents a cache miss
+    /// that should be filled in
+    Missing(File),
+}
+
+pub fn fetch(year: u32, day: u32) -> Entry {
+    let cache_dir = paths::cache_directory().unwrap();
+    std::fs::create_dir_all(&cache_dir).unwrap();
+
+    let filename = format!("{}_{:02}.txt", year, day);
+    let target_cache = cache_dir.join(filename);
+
+    if target_cache.exists() {
+        let f = File::options().read(true).open(target_cache).unwrap();
+        Entry::Cached(f)
+    } else {
+        let f = File::options()
+            .create_new(true)
+            .write(true)
+            .open(target_cache)
+            .unwrap();
+        Entry::Missing(f)
+    }
+}
